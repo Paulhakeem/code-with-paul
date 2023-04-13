d97614a6ed3660aca20df88f6f2cc397597d7446
Paul <101798876+Paulhakeem@users.noreply.github.com>
2023-04-13 08:21:47 +0000

    Services.vue

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -13,7 +13,9 @@
        </div>
      <p class="mr-2 ml-2 select-none">We create websites that are appealing and responsive across all platforms. A website that looks great and will assist you in achieving the company goals you have set.</p>
       <div class="ml-2 mr-2">
+        <a href="https://wa.link/38fuqe" target="_blank">
        <button class="uppercase bg-blue-800 p-2 text-white font-lighter text-md rounded-md mt-4">Order Now</button>
+          </a>
          </div>
     </div>
    <div class="auto-cols-min bg-gray-300 text-center w-80 rounded-md pb-6 cursor-pointer hover:bg-white hover:border hover:border-1 hover:border-gray-400">
@@ -23,7 +25,9 @@
        </div>
      <p class="mr-2 ml-2 select-none">We create a logo that is appealing to all viewers, instantly identifiable, and distinctive.</p>
       <div class="ml-2 mr-2">
+        <a href="https://wa.link/38fuqe" target="_blank">
        <button class="uppercase bg-blue-800 p-2 text-white font-lighter text-md rounded-md mt-16">Order Now</button>
+          </a>
          </div>
     </div>
    <div class="auto-cols-min bg-gray-300 text-center w-80 rounded-md pb-6 cursor-pointer hover:bg-white hover:border hover:border-1 hover:border-gray-400">
@@ -33,7 +37,9 @@
        </div>
      <p class="mr-2 ml-2 select-none">You can provide services to customers at their doorsteps with the use of flyers. They are appropriate for all business types and will enable you to effectively achieve your advertising objectives.</p>
       <div class="ml-2 mr-2">
+        <a href="https://wa.link/38fuqe" target="_blank">
        <button class="uppercase bg-blue-800 p-2 text-white font-lighter text-md rounded-md mt-4">Order Now</button>
+          </a>
          </div>
     </div>
    <div class="auto-cols-min bg-gray-300 text-center w-80 rounded-md pb-6 cursor-pointer hover:bg-white hover:border hover:border-1 hover:border-gray-400">
@@ -44,7 +50,9 @@
      <p class="mr-2 ml-2 select-none">Any type of business can use brochures as a marketing tool to spread the word.
 In contrast to flyers, a brochure is like a book in form.</p>
       <div class="ml-2 mr-2">
+        <a href="https://wa.link/38fuqe" target="_blank">
        <button class="uppercase bg-blue-800 p-2 text-white font-lighter text-md rounded-md mt-24">Order Now</button>
+          </a>
          </div>
     </div>
    <div class="auto-cols-min bg-gray-300 text-center w-80 rounded-md cursor-pointer pb-6 hover:bg-white hover:border hover:border-1 hover:border-gray-400">
@@ -54,7 +62,9 @@ In contrast to flyers, a brochure is like a book in form.</p>
         </div>
      <p class="mr-2 ml-2 select-none">Business cards include information about you and the organization you work for. If you have a business card, you can use it to introduce yourself to people so they can get in touch with you and learn more about what you do, which will bring in clients.</p>
             <div class="ml-2 mr-2">
+              <a href="https://wa.link/38fuqe" target="_blank">
        <button class="uppercase bg-blue-800 p-2 text-white font-lighter text-md rounded-md mt-4">Order Now</button>
+                </a>
          </div>
     </div>
    <div class="auto-cols-min bg-gray-300 text-center w-80 rounded-md cursor-pointer pb-6 hover:bg-white hover:border hover:border-1 hover:border-gray-400">
@@ -64,7 +74,9 @@ In contrast to flyers, a brochure is like a book in form.</p>
       </div>
      <p class="mr-2 ml-2 select-none">Roll-up banners are a common marketing tool used by businesses to draw in clients. And it will continue to be one of the most successful ways to draw customers.</p>
             <div class="ml-2 mr-2">
+              <a href="https://wa.link/38fuqe" target="_blank">
        <button class="uppercase bg-blue-800 p-2 text-white font-lighter text-md rounded-md mt-16">Order Now</button>
+                </a>
          </div>
     </div>
      <div class="auto-cols-min bg-gray-300 text-center w-80 rounded-md cursor-pointer pb-6 hover:bg-white hover:border hover:border-1 hover:border-gray-400">
@@ -74,7 +86,9 @@ In contrast to flyers, a brochure is like a book in form.</p>
       </div>
      <p class="mr-2 ml-2 select-none">Advertising with posters is an effective way to focus your message and deliver it to a specific audience. A strategically placed company advertising poster has a lot of room for creativity, applications, and reach. You can select to mail a poster directly to the people you wish to reach.</p>
        <div class="ml-2 mr-2">
+         <a href="https://wa.link/38fuqe" target="_blank">
        <button class="uppercase bg-blue-800 p-2 text-white font-lighter text-md rounded-md mt-4">Order Now</button>
+           </a>
          </div>
     </div>
    </div>
